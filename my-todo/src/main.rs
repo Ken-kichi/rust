975d6e39cc7212048e779a99c5969d7b8e94a3ad
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2022-10-23 08:34:12 +0000

    ファイル分割

--- my-todo/src/main.rs
+++ my-todo/src/main.rs
@@ -1,111 +1,27 @@
-// Webアプリで使用するライブラリをインポートする
-use anyhow::Context;
+// ファイルの読み込み
+mod handlers;
+mod repositories;
+
+// 別ファイルのクレートを読み込み
+use crate::repositories::{TodoRepository, TodoRepositoryForMemory};
+// パッケージの追加
 use axum::{
     extract::Extension,
-    http::StatusCode,
-    response::IntoResponse,
     routing::{get, post},
-    Json, Router,
+    Router
 };
-use serde::{Deserialize, Serialize};
-use thiserror::Error;
-use std::{net::SocketAddr, error};
-use std::{
-    collections::HashMap,
-    env,
-    sync::{Arc, RwLock}
-};
-
-#[derive(Debug, Error)]
-enum RepositoryError {
-    #[error("NotFound, id is {0}")]
-    NotFound(i32),
-}
-
-pub trait TodoRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
-    fn create(&self, payload: CreateTodo) -> Todo;
-    fn find(&self, id: i32) -> Option<Todo>;
-    fn all(&self) -> Vec<Todo>;
-    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo>;
-    fn delete(&self, id: i32) -> anyhow::Result<()>;
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub struct Todo {
-    id: i32,
-    text: String,
-    completed: bool,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub struct CreateTodo {
-    text: String,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub struct UpdateTodo {
-    text: Option<String>,
-    completed: Option<bool>,
-}
-
-impl Todo {
-    pub fn new(id: i32, text: String) -> Self {
-        Self {
-            id,
-            text,
-            completed: false,
-        }
-    }
-}
-
-type TodoDatas = HashMap<i32, Todo>;
-
-#[derive(Debug, Clone)]
-pub struct TodoRepositoryForMemory {
-    store:Arc<RwLock<TodoDatas>>,
-}
-
-impl TodoRepositoryForMemory {
-    pub fn new() -> Self {
-        TodoRepositoryForMemory {
-             store: Arc::default(), 
-            }
-    }
-}
-
-impl TodoRepository for TodoRepositoryForMemory {
-    fn create(&self, payload: CreateTodo) -> Todo {
-        todo!();
-    }
-
-    fn find(&self, id: i32) -> Option<Todo> {
-        todo!();
-    }
-
-    fn all(&self) -> Vec<Todo> {
-        todo!();
-    }
-
-    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
-        todo!();
-    }
-
-    fn delete(&self, id: i32) -> anyhow::Result<()> {
-        todo!();
-    }
-
-}
-
+use handlers::create_todo;
+use std::net::SocketAddr;
+use std::{env, sync::Arc};
 
 
 #[tokio::main]
 async fn main() {
-    // loggingを初期化する
+    // loggingの初期化
     let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
 
-    // Webサーバーの準備
     let repository = TodoRepositoryForMemory::new();
     let app = create_app(repository);
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
@@ -116,7 +32,6 @@ async fn main() {
         .unwrap();
 }
 
-// ルーターの設定
 fn create_app<T: TodoRepository>(repository: T) -> Router {
     Router::new()
         .route("/", get(root))
@@ -124,42 +39,23 @@ fn create_app<T: TodoRepository>(repository: T) -> Router {
         .layer(Extension(Arc::new(repository)))
 }
 
-pub async fn create_todo<T: TodoRepository>(
-    Json(payload): Json<CreateTodo>,
-    Extension(repository): Extension<Arc<T>>,
-) -> impl IntoResponse {
-    let todo = repository.create(payload);
-
-    (StatusCode::CREATED, Json(todo))
-}
-
-// ”/”で表示される内容を記載
 async fn root() -> &'static str {
     "Hello World"
 }
 
-// 情報の出力の仕方を設定
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-struct User {
-    id: u64,
-    username: String,
-}
-
 #[cfg(test)]
 mod test {
-    use std::io::empty;
-
     use super::*;
     use axum::{body::Body, http::Request};
     use tower::ServiceExt;
 
     #[tokio::test]
     async fn should_return_hello_world() {
-        let repository = TodoRepositoryForMemory::new();
         let req = Request::builder().uri("/").body(Body::empty()).unwrap();
+        let repository = TodoRepositoryForMemory::new();
         let res = create_app(repository).oneshot(req).await.unwrap();
         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
         let body: String = String::from_utf8(bytes.to_vec()).unwrap();
-        assert_eq!(body, "Hello World")
+        assert!(body, "Hello World");
     }
 }
